5aa019c09141b0656fd9f97ab886e095f00f0167
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-03-22 10:06:34 -0400

    Add basic number button function
    When clicked the number buttons will type and add their number
    to the screen

--- script.js
+++ script.js
@@ -32,3 +32,34 @@ function multi(num1, num2){
 function division(num1, num2){
     return +num1 / +num2;
 }
+
+let screenText = document.querySelector(".screenText");
+let oneButton = document.querySelector("#numberOne");
+oneButton.addEventListener("click", () => screenText.innerText += "1")
+
+let twoButton = document.querySelector("#numberTwo");
+twoButton.addEventListener("click", () => screenText.innerText += "2")
+
+let threeButton = document.querySelector("#numberThree");
+threeButton.addEventListener("click", () => screenText.innerText += "3")
+
+let fourButton = document.querySelector("#numberFour");
+fourButton.addEventListener("click", () => screenText.innerText += "4")
+
+let fiveButton = document.querySelector("#numberFive");
+fiveButton.addEventListener("click", () => screenText.innerText += "5")
+
+let sixButton = document.querySelector("#numberSix");
+sixButton.addEventListener("click", () => screenText.innerText += "6")
+
+let sevenButton = document.querySelector("#numberSeven");
+sevenButton.addEventListener("click", () => screenText.innerText += "7")
+
+let eightButton = document.querySelector("#numberEight");
+eightButton.addEventListener("click", () => screenText.innerText += "8")
+
+let nineButton = document.querySelector("#numberNine");
+nineButton.addEventListener("click", () => screenText.innerText += "9")
+
+let zeroButton = document.querySelector("#numberZero");
+zeroButton.addEventListener("click", () => screenText.innerText += "0")
